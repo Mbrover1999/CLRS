20d82fadb3d3adca2f381c9a29109f9ae9363748
Michael Brover <lepovan1999@gmail.com>
2025-12-15 19:01:35 +0200

    Merge sort

--- MergeSort.py
+++ MergeSort.py
@@ -8,15 +8,15 @@ class MergeSort:
         if p >= r:
             return
         q = int((p + r) / 2)
-        self.merge_sort(p , q)
-        self.merge_sort(q + 1, r)
-        self.merge(p, q, r)
+        self.merge_sort(p , q) # divide the left portion of the array
+        self.merge_sort(q + 1, r) # divide the right portion of the array
+        self.merge(p, q, r) # merge the arrays
 
     def merge(self,  p : int, q : int, r : int):
-        n_l = q - p + 1
-        n_r = r - q
-        left_arr = self.arr[p : q]
-        right_arr = self.arr[q : r]
+        n_l = q - p + 1 # left array size
+        n_r = r - q # right array size
+        left_arr = self.arr[p : q + 1] # copy to the left array
+        right_arr = self.arr[q + 1 : r + 1] # copy to the right array
         i = 0
         j = 0
         k = p
@@ -27,7 +27,7 @@ class MergeSort:
             else:
                 self.arr[k] = right_arr[j]
                 j += 1
-        k += 1
+            k += 1
         while i < n_l:
             self.arr[k] = left_arr[i]
             i += 1
