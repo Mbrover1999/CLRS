393f18bcb8f04eb8981b4ebcd3220f5db3aa0cde
Michael Brover <lepovan1999@gmail.com>
2025-12-15 17:45:21 +0200

    two sum

---
+++ MergeSort.py
@@ -0,0 +1,46 @@
+class MergeSort:
+
+    def __init__(self, arr : list[int]):
+        self.arr = arr
+
+
+    def merge_sort(self, p : int, r : int):
+        if p >= r:
+            return
+        q = int((p + r) / 2)
+        self.merge_sort(p , q)
+        self.merge_sort(q + 1, r)
+        self.merge(p, q, r)
+
+    def merge(self,  p : int, q : int, r : int):
+        n_l = q - p + 1
+        n_r = r - q
+        left_arr = self.arr[p : q]
+        right_arr = self.arr[q : r]
+        i = 0
+        j = 0
+        k = p
+        while i < n_l and j < n_r:
+            if left_arr[i] <= right_arr[j]:
+                self.arr[k] = left_arr[i]
+                i += 1
+            else:
+                self.arr[k] = right_arr[j]
+                j += 1
+        k += 1
+        while i < n_l:
+            self.arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        while j < n_r:
+            self.arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+def main():
+    test = MergeSort([2, 4, 6, 7, 1 ,2, 3, 5])
+    test.merge_sort(0, 7)
+    for num in test.arr:
+        print(num)
+
+main()
